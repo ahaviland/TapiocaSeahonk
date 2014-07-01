9d2835d08d6f5b778fda9b12c7b3def529fbd2a9
ahaviland <ahaviland1@gmail.com>
2014-07-01 05:07:15 -0400

    Update euler.rb

--- euler.rb
+++ euler.rb
@@ -1,29 +1,22 @@
-def even_fib_below cap                            # sums even numbers in the fibonacci sequence below a given cap
-	i=1                                       # (answers seem to come up a little short. Haven't had a good 
-	sum=0                                     # look at WHY yet.)
-	evens = []
-	while i<cap
-		if i % 2 == 0
-			evens << i
-		end
-		i+=i		
-	end
-	evens.each do |even_fib|
-		sum += even_fib
-	end
-	sum
-end
+=begin
+I've posted some slightly general versions of project euler problems here. I've tried to preserve
+formatting, but since I've never used Github before I'm pasting my methods in through my browser
+in the interest of spending my time practicing Ruby and not Github, so indentation might be slightly wonky.
 
-##############################################################
 
-def three_or_five_mults cap                        #sums all multiples of 3 or 5 below 1000
-	nums=[]
-	sum=0
-	for num in 1...1000
-		if num % 3 == 0 || num % 5 == 0
-			nums << num
-		end	
-	end
+I think I could make these first 2 problems truely general with Inject and the Splat Operator, but I'm not 
+quite there yet.
+=end
+
+
+def three_or_five_mults (bottom, top, first_divisor, second_divisor)  # sums numbers divisible by either
+  nums=[]                                                             # of two numbers within a given range
+  sum=0                                                               
+    for num in bottom...top
+      if num % first_divisor == 0 || num % second_divisor == 0
+        nums << num
+      end	
+    end
 
   nums.each do |mult|
     sum+=mult
@@ -31,4 +24,21 @@ def three_or_five_mults cap                        #sums all multiples of 3 or 5
 
 return sum
 
+end
+################################################################################
+
+def even_fib_below cap                            # sums even numbers in the fibonacci sequence below a given cap
+	i=1                                       # (answers seem to come up a little short. Haven't had a good 
+	sum=0                                     # look at WHY yet.)
+	evens = []
+	  while i<cap
+	    if i % 2 == 0
+	      evens << i
+	    end
+	    i+=i		
+	  end
+      evens.each do |even_fib|
+        sum += even_fib
+      end
+  sum
 end
