6cfed4a8828746ed18e7823e85ce53bb01906144
ahaviland <ahaviland1@gmail.com>
2014-07-01 04:22:01 -0400

    Update euler.rb

--- euler.rb
+++ euler.rb
@@ -1,4 +1,4 @@
-def even_fib_below cap               #finds even numbers in fibonacci sequence up to a given limit          
+def even_fib_below cap                            #finds even numbers in fibonacci sequence up to a given limit          
 	i=1                                     
 	evens=[]                                
 	while i <= cap                           
@@ -10,3 +10,24 @@ def even_fib_below cap               #finds even numbers in fibonacci sequence u
 	end                                    
 	return [evens]                             
 end                
+
+##############################################################
+
+def three_or_five_mults cap                        #sums all multiples of 3 or 5 below 1000
+	nums=[]
+	sum=0
+	for num in 1...1000
+		if num % 3 == 0 || num % 5 == 0
+			nums << num
+		end	
+	end
+
+  nums.each do |mult|
+    sum+=mult
+  end
+
+return sum
+
+end
+
+p three_or_five_mults(1000)
